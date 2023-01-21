ff5a6a455cf98db71435b3d7798d5008b9cddfa3
Pranav Bijapur <bijapurpranav@gmail.com>
2023-01-21 13:22:41 +0530

    strategy pattern for powercap interface to Agent

--- agent/agent.cpp
+++ agent/agent.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include "toml.hpp"
+#include "../powercap/dummy_powercap.cpp"
 
 class Agent {
+private:
+    std::unique_ptr<PowercapInterface> powercap_interface_;
 
+public:
+    explicit Agent(std::unique_ptr<PowercapInterface> &&powercapInterface = {}) : powercap_interface_(std::move(powercapInterface)) {
+
+    }
 };
 
 int main(int argc, char *argv[]) {
-    auto config = toml::parse_file( "../agent/config.toml" );
+    auto config = toml::parse_file("../agent/config.toml");
 
-    Agent agent = Agent();
+    Agent agent(std::make_unique<DummyPowercap>());
     std::cout << "agent launched\n";
 }
